652f40a6ccfbbe4b071b2763b501c2a51a9310df
Gabriel Pedrosa <gabrielaraujopedrosa@gmail.com>
2023-05-26 09:48:02 -0300

    Added Logout Token button

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './Dashboard.css'
+import { useNavigate } from 'react-router-dom'
 
 const Dashboard = ({token}) => {
-  return (
+  
+  let navigate = useNavigate();
+
+  function handleLogout(){
+    sessionStorage.removeItem('token')
+    localStorage.removeItem(key)
+    navigate('/')
+  }
+
+  return ( 
     <div>
       <h1>Dashboard</h1>
       <p>Seja bem vindo, {token.user.user_metadata.username}</p>
+      <button onClick={handleLogout}> Logout</button>
     </div>
   )
 }
